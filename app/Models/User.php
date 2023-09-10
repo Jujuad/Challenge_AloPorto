7b4f9425a6a016201c29eaadce04758734821ea0
julia.andrade-at-538590928221 <qoTQt0Cryp6/ysKmwHlONj6KyLQNdpMjVwl0qeqlNns=>
2023-09-10 02:14:03 -0300

    Estrutura inicial de models e migrations + atualizacao de user + models caminhao, guincho e veiculo completos

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'status',
+        'login_status',
+        'register_date',
+        'protocolo_seguro'
     ];
 
     /**
